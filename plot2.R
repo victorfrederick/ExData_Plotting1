b2b6227744d297e24585bd9fee97bf9939df1b77
victorfrederick <42483541+victorfrederick@users.noreply.github.com>
2018-11-23 14:36:01 +0900

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,19 @@
+#download and unzip data
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip(('./data/household_power_consumption.zip', exdir = './data')
+      
+#create files and read data
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+#Plot 2
+#convert date variable to Date class
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+#plot2
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot2.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,19 @@
+#download and unzip data
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip(('./data/household_power_consumption.zip', exdir = './data')
+      
+#create files and read data
+files <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+
+#Plot 3
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot3.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+dev.off()
